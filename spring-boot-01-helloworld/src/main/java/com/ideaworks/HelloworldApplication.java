b358b58cdc09163d491b73a54804f0f700fe11d8
浮巷 <599252594@qq.com>
2020-04-29 11:21:12 +0800

    spring-boot-01-helloworld

---
+++ spring-boot-01-helloworld/src/main/java/com/ideaworks/HelloworldApplication.java
@@ -0,0 +1,11 @@
+package com.ideaworks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloworldApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(HelloworldApplication.class,args);
+    }
+}
---
+++ spring-boot-01-helloworld/src/main/java/com/ideaworks/controller/HelloController.java
@@ -0,0 +1,14 @@
+package com.ideaworks.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HelloController {
+    @ResponseBody
+    @GetMapping("/hello")
+    public String helloworld(){
+        return "hello world!";
+    }
+}
